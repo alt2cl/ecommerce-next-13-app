b9610b572a226b281cb580457f0b8e078c49a152
alt2cl <aldo@alt2.cl>
2023-06-23 10:09:35 -0400

    ajustes config de nextjs

--- next.config.js
+++ next.config.js
@@ -33,32 +33,40 @@ module.exports = {
   env: {
     NEXT_PUBLIC_STRAPI_URL: process.env.NEXT_PUBLIC_STRAPI_URL,
   },
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents:
-      false |
+  async rewrites() {
+    return [
       {
-        // Enabled by default in development, disabled in production to reduce file size,
-        // setting this will override the default for all environments.
-        displayName: true,
-        // Enabled by default.
-        ssr: true,
-        // Enabled by default.
-        //fileName: true,
-        // Empty by default.
-        //topLevelImportPaths?: string[] ,
-        // Defaults to ["index"].
-        //meaninglessFileNames?: string[],
-        // Enabled by default.
-        //cssProp: boolean,
-        // Empty by default.
-        //namespace?: string,
-        // Not supported yet.
-        minify: true,
-        // Not supported yet.
-        //transpileTemplateLiterals?: boolean,
-        // Not supported yet.
-        //pure?: boolean,
+        source: "/api/clientes",
+        destination: `${process.env.NEXT_PUBLIC_STRAPI_URL}/clientes`,
       },
+    ];
   },
+  // compiler: {
+  //   // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+  //   styledComponents:
+  //     false |
+  //     {
+  //       // Enabled by default in development, disabled in production to reduce file size,
+  //       // setting this will override the default for all environments.
+  //       //displayName: true,
+  //       // Enabled by default.
+  //       //ssr: true,
+  //       // Enabled by default.
+  //       //fileName: true,
+  //       // Empty by default.
+  //       //topLevelImportPaths?: string[] ,
+  //       // Defaults to ["index"].
+  //       //meaninglessFileNames?: string[],
+  //       // Enabled by default.
+  //       //cssProp: boolean,
+  //       // Empty by default.
+  //       //namespace?: string,
+  //       // Not supported yet.
+  //       //minify: true,
+  //       // Not supported yet.
+  //       //transpileTemplateLiterals?: boolean,
+  //       // Not supported yet.
+  //       //pure?: boolean,
+  //     },
+  // },
 };
