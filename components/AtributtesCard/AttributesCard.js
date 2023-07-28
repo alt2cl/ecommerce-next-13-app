5f709fcb204e17fd706d96d672bdf50dd0f38382
alt2cl <aldo@alt2.cl>
2023-07-28 09:06:02 -0400

    ajustes list attr card

--- components/AtributtesCard/AttributesCard.js
+++ components/AtributtesCard/AttributesCard.js
@@ -7,8 +7,8 @@ function AttributesCard({ data }) {
   return (
     <>
       <HeadSection
-        titulo={data.attributes.titulo}
-        subtitulo={data.attributes.subtitulo}
+        titulo={data.attributes.title}
+        subtitulo={data.attributes.subtitle}
         center
       />
       <div
@@ -16,21 +16,21 @@ function AttributesCard({ data }) {
           data.attributes.cols ? cantCol : "lg:grid-cols-3"
         }`}
       >
-        {data.attributes?.Atributos?.map((item) => {
+        {data.attributes?.atribute?.map((item) => {
           return (
             <div
               className="flex flex-col gap-3 mb-5 lg:mb-16 drop-shadow-md px-4 py-4 rounded-lg bg-white items-center"
               key={`att-${item.id}`}
             >
-              <h3 className="uppercase text-xl">{item.titulo}</h3>
-              <h5 className="text-lg">{item.subtitulo}</h5>
-              <p className="text-gray-600">{item.descripcion}</p>
-              {item.link && item.titulolink && (
+              <h3 className="uppercase text-xl">{item.title}</h3>
+              <h5 className="text-lg">{item.subtitle}</h5>
+              <p className="text-gray-600">{item.description}</p>
+              {item.url && item.titlebutton && (
                 <Link
-                  href={item.link}
+                  href={item.url}
                   className="rounded-md text-center p-2 bg-orange-500 hover:bg-slate-900 text-white"
                 >
-                  {item.titulolink}
+                  {item.titlebutton}
                 </Link>
               )}
             </div>
