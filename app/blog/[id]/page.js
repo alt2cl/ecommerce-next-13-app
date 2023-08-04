1505ed25d805dc611161ca0ea894ad148f3b0db0
alt2cl <aldo@alt2.cl>
2023-08-04 00:00:50 -0400

    sidebar articulo

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,6 +1,7 @@
 //import PostsList from "../postsList";
 import Image from "next/image";
 import markdownToHtml from "@/app/utils/markdownToHtml";
+import ThumbList from "@/components/ThumbList/ThumbList";
 
 const getPost = (id) => {
   return fetch(
@@ -9,8 +10,20 @@ const getPost = (id) => {
   ).then((res) => res.json());
 };
 
+async function getDataSidebar() {
+  let res = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/featured-sidebars/?populate=products.category_products&populate=products.cover`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
 export default async function PostPage({ params: { id } }) {
   const post = await getPost(id);
+  const { data: datasidebar } = await getDataSidebar();
+
   const {
     title: titulo,
     content: contenido,
@@ -50,6 +63,14 @@ export default async function PostPage({ params: { id } }) {
         </main>
         <aside className=" block col-span-12 lg:col-span-4">
           {/* <PostsList shortPost={true} /> */}
+          {datasidebar?.map((item) => {
+            return (
+              <ThumbList
+                headtext={item.attributes.title}
+                promise={item.attributes.products.data}
+              />
+            );
+          })}
         </aside>
       </div>
     </div>
