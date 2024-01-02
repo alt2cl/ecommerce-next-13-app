86610e78be74c787b7f8325c14d950093bf04809
alt2cl <aldo@alt2.cl>
2024-01-02 11:00:07 -0300

    ajustes cortes de imagenes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -34,7 +34,10 @@ export default async function Footer() {
                                 <div className="w-full lg:w-1/4 px-5 pb-10">
                                     {item.attributes.logo.data?.attributes.url ? <Image
                                         className=" max-w-sm mb-6"
-                                        src={item.attributes.logo.data?.attributes.url}
+                                        src={item.attributes.logo.data?.attributes.url.replace(
+                                            "upload/",
+                                            "upload/c_fill,h_112,w_496/"
+                                        )}
                                         width={248}
                                         height={56}
                                         alt="Logo"
